c1c522bba459aa3d90db3ed6367e81f0abe8a7f5
Raymond Le <raymond.le@nexcel.vn>
2017-05-18 17:53:10 +0700

    6.2.4 Format validation

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/ahihi'
+  root 'static_pages#home'
 
   get  '/help',    to: 'static_pages#help', as: 'help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  
-  root 'static_pages#home'
+  get  '/signup',  to: 'users#new'
+
+  get 'static_pages/ahihi'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
